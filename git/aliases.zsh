66c793b7a004320ecf20d484bf5a389592153425
Ryan Blakeley <rbblakeley@gmail.com>
2018-09-17 08:14:18 -0600

    Update git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -7,8 +7,9 @@ then
 fi
 
 # The rest of my fun git aliases
-alias gl='git pull --prune'
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias gpull='git pull --prune'
+alias glog='git log'
+# alias othergitlog = '"git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
 
 # Remove `+` and `-` from start of diff lines; just rely upon color.
@@ -16,11 +17,11 @@ alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
 
 alias gc='git commit -m' # requires you to type a commit message
 alias gco='git checkout'
-alias gcb='git copy-branch-name'
+# alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
-alias ga='git add .'
+# alias ga='git add .'
 alias gac='git add -A && git commit -m'
 alias ge='git-edit-new'
-alias gp='git push'
+# alias gp='git push'
 alias grm='git rm $(git ls-files --deleted)'
